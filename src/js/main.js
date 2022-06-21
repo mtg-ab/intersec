36d6990571708a9377fe97e8472af2eb3620519f
Andrew Bouvet <abouvet@makethegrade.fr>
2022-06-21 11:58:09 +0200

    Hero homepage

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,60 @@
  * Main.js
  *
  * @since 1.0.0
+ */
+
+/*Import Plugin
+var Isotope = require('isotope-layout');
+var Flickity = require('flickity');
+var InfiniteScroll = require('infinite-scroll');
+var Sticky = require('sticky-js');
 */
 
-/*
-const { gsap } = require("gsap/dist/gsap");
+var Flickity = require('flickity');
+var Flickity = require('flickity-fade');
+import Splitting from "splitting";
+import { gsap } from "gsap";
+//import * as THREE from 'three';
+
+
+
+Splitting({
+    target: "h2, [data-splitting], h1, div.testimonial p",
+    by: "chars",
+    key: null
+}); 
+
+
 const { ScrollSmoother } = require('gsap/dist/ScrollSmoother.js');
 const { ScrollTrigger } = require('gsap/dist/ScrollTrigger');
 gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
-*/
 
-/*
 jQuery( document ).ready(function() {
     
     let smoother = ScrollSmoother.create({
         wrapper: "div#smooth-wrapper",
         content: "div#smooth-content",
-        smooth: 1.7,
+        smooth: 1,
         effects: true
     });
     
 })
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
